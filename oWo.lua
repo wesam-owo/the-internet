0e03117bf1a68dd55883aa61497f1a7d406e9eee
wesam-owo <wsammghyd147@gmail.com>
2025-07-29 00:09:21 +0300

    Create oWo.lua
    لا تحذف

---
+++ oWo.lua
@@ -0,0 +1,10 @@
+-- اسم السكربت: oWo
+-- الميزة: عرض رسالة ترحيبية في روبلوكس
+
+local player = game.Players.LocalPlayer
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "oWo Script",
+    Text = "تم تشغيل السكربت بنجاح!",
+    Duration = 5
+})
